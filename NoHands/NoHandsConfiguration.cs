47a85e27255c97fcf34b81d3a834bd0b40e7b318
ElMargaritox <alexisguzman0303@gmail.com>
2021-09-19 17:32:30 -0300

    Agregar archivos de proyecto.

---
+++ NoHands/NoHandsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rocket.API;
+
+namespace NoHands
+{
+    public class NoHandsConfiguration : IRocketPluginConfiguration
+    {
+        public string Permission_Ignore { get; set; }
+
+        public void LoadDefaults()
+        {
+            Permission_Ignore = "NoHands.Ignore";
+        }
+    }
+}
---
+++ NoHands/NoHandsPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace NoHands
+{
+    public class NoHandsPlugin : RocketPlugin<NoHandsConfiguration>
+    {
+        public NoHandsPlugin Instance { get; set; }
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("NoHands. Cargado Correctamente");
+            UnturnedPlayerEvents.OnPlayerUpdateGesture += UnturnedPlayerEvents_OnPlayerUpdateGesture;
+        }
+
+        private void UnturnedPlayerEvents_OnPlayerUpdateGesture(Rocket.Unturned.Player.UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+        {
+          if(gesture == UnturnedPlayerEvents.PlayerGesture.InventoryOpen)
+            {
+                if (!player.HasPermission(Configuration.Instance.Permission_Ignore))
+                {
+                    player.Inventory.items[2].resize(0, 0);
+                }
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("NoHands. Deshabilitado Correctamente");
+            UnturnedPlayerEvents.OnPlayerUpdateGesture -= UnturnedPlayerEvents_OnPlayerUpdateGesture;
+
+        }
+    }
+}
